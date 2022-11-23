d478acc1af19314c610f468e5e36dbe0a2289da6
Sofia N. Lima <me@dzshn.xyz>
2022-11-23 00:16:02 -0300

    improve source code readability :trolleybus:

--- uwu/magic.py
+++ uwu/magic.py
@@ -2,10 +2,10 @@ import abc
 import ctypes
 import dataclasses
 import re
+import opcode
 import sys
 import types
 from typing import Any, Literal, Union
-from opcode import cmp_op, opmap as om, opname, hasjabs, hasjrel
 
 from uwu.stack import Stack
 
@@ -49,31 +49,17 @@ class Name(Match):
 class Op(Match):
     value: Union[str, re.Pattern]
 
+    def __post_init__(self):
+        self._re = re.compile(re.escape(self.value).replace("\\*", ".*"))
+
     def matches(self, code: types.CodeType, index: int) -> bool:
-        op = opname[code.co_code[index]]
-        if isinstance(self.value, str):
-            return op == self.value
-        return self.value.fullmatch(op)
+        op = opcode.opname[code.co_code[index]]
+        return self._re.fullmatch(op)
 
 
 class Always(Match):
     def matches(self, code: types.CodeType, index: int) -> bool:
         return True
-
-
-HEADER = [
-    # import uwu.magic
-    om["LOAD_CONST"],  Const(0),
-    om["LOAD_CONST"],  Const(None),
-    om["IMPORT_NAME"], Name("uwu.magic"),
-    om["STORE_NAME"],  Name("uwu"),
-
-    # haiiiiii <3
-    om["LOAD_NAME"],   Name(re.compile(r"haii+")),
-    om["LOAD_CONST"],  Const(3),
-    om["COMPARE_OP"],  cmp_op.index("<"),
-    om["POP_TOP"],     0,
-]
 
 
 def explod(message: str):
@@ -98,6 +84,22 @@ def match_code(code: types.CodeType, index: int, query: list[Match]):
 if __name__ == "__main__":
     explod("no")
 
+locals().update(opcode.opmap) # 🚎
+
+HEADER = [
+    # import uwu.magic
+    LOAD_CONST,  Const(0),
+    LOAD_CONST,  Const(None),
+    IMPORT_NAME, Name("uwu.magic"),
+    STORE_NAME,  Name("uwu"),
+
+    # haiiiiii <3
+    LOAD_NAME,   Name(re.compile(r"haii+")),
+    LOAD_CONST,  Const(3),
+    COMPARE_OP,  opcode.cmp_op.index("<"),
+    POP_TOP,     0,
+]
+
 frame = sys._getframe(1)
 while "importlib" in frame.f_code.co_filename:
     frame = frame.f_back
@@ -109,70 +111,70 @@ if not match_code(code, 0, HEADER):
     explod("ure a meanie")
 
 i = len(HEADER)
-new_code = bytearray([om["NOP"], 69] * (i // 2) + [om["POP_TOP"], 0])
+new_code = bytearray([NOP, 69] * (i // 2) + [POP_TOP, 0])
 labels = {}
 jumps = {}
 while i < len(bytecode):
     if nop := match_code(code, i, [
-        om["LOAD_NAME"], Name("o"),
-        om["LOAD_NAME"], Name("w"),
-        om["BINARY_MATRIX_MULTIPLY"], 0,
-        om["LOAD_NAME"], Name("o"),
-        om["BINARY_MATRIX_MULTIPLY"], 0,
+        LOAD_NAME, Name("o"),
+        LOAD_NAME, Name("w"),
+        BINARY_MATRIX_MULTIPLY, 0,
+        LOAD_NAME, Name("o"),
+        BINARY_MATRIX_MULTIPLY, 0,
     ]):
         i += len(nop)
         while not match_code(code, i, [
-            om["CALL_FUNCTION"], Always()
+            CALL_FUNCTION, Always()
         ]):
             i += 2
         i += 4
     elif push := match_code(code, i, [
-        om["LOAD_NAME"], Name("o"),
-        om["LOAD_NAME"], Name("w"),
-        om["BINARY_XOR"], 0,
-        om["LOAD_NAME"], Name("o"),
-        om["BINARY_XOR"], 0,
-        Op(re.compile(r"LOAD_.*")), Always(),
-        om["COMPARE_OP"], cmp_op.index(">"),
-        om["POP_TOP"], 0,
+        LOAD_NAME, Name("o"),
+        LOAD_NAME, Name("w"),
+        BINARY_XOR, 0,
+        LOAD_NAME, Name("o"),
+        BINARY_XOR, 0,
+        Op("LOAD_*"), Always(),
+        COMPARE_OP, opcode.cmp_op.index(">"),
+        POP_TOP, 0,
     ]):
         new_code.append(push[-6])
         new_code.append(push[-5])
         i += len(push)
     elif store := match_code(code, i, [
-        Op(re.compile(r"LOAD_.*")), Always(),
-        om["LOAD_NAME"], Name("o"),
-        om["LOAD_NAME"], Name("w"),
-        om["BINARY_XOR"], 0,
-        om["LOAD_NAME"], Name("o"),
-        om["BINARY_XOR"], 0,
-        om["COMPARE_OP"], cmp_op.index("<"),
-        om["POP_TOP"], 0,
+        Op("LOAD_*"), Always(),
+        LOAD_NAME, Name("o"),
+        LOAD_NAME, Name("w"),
+        BINARY_XOR, 0,
+        LOAD_NAME, Name("o"),
+        BINARY_XOR, 0,
+        COMPARE_OP, opcode.cmp_op.index("<"),
+        POP_TOP, 0,
     ]):
-        new_code.append(om[opname[store[0]].replace("LOAD_", "STORE_")])
+        new_code.append(opcode.opmap[opcode.opname[store[0]].replace("LOAD_", "STORE_")])
         new_code.append(store[1])
         i += len(store)
     elif arbitrary := match_code(code, i, [
-        om["LOAD_NAME"], Name("o"),
-        om["LOAD_NAME"], Name("w"),
-        om["BINARY_FLOOR_DIVIDE"], 0,
-        om["LOAD_NAME"], Name("o"),
-        om["BINARY_FLOOR_DIVIDE"], 0,
+        LOAD_NAME, Name("o"),
+        LOAD_NAME, Name("w"),
+        BINARY_FLOOR_DIVIDE, 0,
+        LOAD_NAME, Name("o"),
+        BINARY_FLOOR_DIVIDE, 0,
     ]):
         i += len(arbitrary)
         j = i
         while not match_code(code, j, [
-            om["BINARY_SUBSCR"], 0,
-            om["POP_TOP"], 0,
+            BINARY_SUBSCR, 0,
+            POP_TOP, 0,
         ]):
             if segment := match_code(code, j, [
-                om["LOAD_NAME"], Always(),
-                om["LOAD_NAME"], Always(),
-                om["BINARY_MATRIX_MULTIPLY"], 0,
+                LOAD_NAME, Always(),
+                LOAD_NAME, Always(),
+                BINARY_MATRIX_MULTIPLY, 0,
             ]):
-                op = om[code.co_names[segment[1]]]
+                op = opcode.opmap[code.co_names[segment[1]]]
                 value = segment[3]
-                if op in hasjabs or op in hasjrel:
+                if op in opcode.hasjabs or op in opcode.hasjrel:
                     label = code.co_names[value]
                     if (value := labels.get(label)) is None:
                         value = 0
@@ -182,42 +184,42 @@ while i < len(bytecode):
                 new_code.append(value)
                 j += len(segment)
             elif segment := match_code(code, j, [
-                om["LOAD_NAME"], Always(),
-                om["LOAD_CONST"], Always(),
-                om["BINARY_MATRIX_MULTIPLY"], 0,
+                LOAD_NAME, Always(),
+                LOAD_CONST, Always(),
+                BINARY_MATRIX_MULTIPLY, 0,
             ]):
-                op = om[code.co_names[segment[1]]]
+                op = opcode.opmap[code.co_names[segment[1]]]
                 value = code.co_consts[segment[3]]
-                if op == om["COMPARE_OP"]:
-                    value = cmp_op.index(value)
+                if op == COMPARE_OP:
+                    value = opcode.cmp_op.index(value)
                 new_code.append(op)
                 new_code.append(value)
                 j += len(segment)
             elif segment := match_code(code, j, [
-                om["LOAD_NAME"], Always(),
+                LOAD_NAME, Always(),
             ]):
-                new_code.append(om[code.co_names[segment[1]]])
+                new_code.append(opcode.opmap[code.co_names[segment[1]]])
                 new_code.append(0)
                 j += len(segment)
-            elif match_code(code, j, [om["BUILD_TUPLE"], Always()]):
+            elif match_code(code, j, [BUILD_TUPLE, Always()]):
                 j += 2
             else:
                 explod("invalid code")
         i = j + 4
     elif label := match_code(code, i, [
-        om["LOAD_NAME"], Name("o"),
-        om["LOAD_NAME"], Name("w"),
-        om["BINARY_XOR"], 0,
-        om["LOAD_NAME"], Name("o"),
-        om["BINARY_XOR"], 0,
-        om["LOAD_NAME"], Always(),
-        om["BINARY_TRUE_DIVIDE"], 0,
-        om["POP_TOP"], 0,
+        LOAD_NAME, Name("o"),
+        LOAD_NAME, Name("w"),
+        BINARY_XOR, 0,
+        LOAD_NAME, Name("o"),
+        BINARY_XOR, 0,
+        LOAD_NAME, Always(),
+        BINARY_TRUE_DIVIDE, 0,
+        POP_TOP, 0,
     ]):
         target = len(new_code) // 2
         labels[code.co_names[label[-5]]] = target
         for x in jumps.get(code.co_names[label[-5]], set()):
-            if new_code[x] in hasjrel:
+            if new_code[x] in opcode.hasjrel:
                 new_code[x + 1] = target - x // 2 - 1
             else:
                 new_code[x + 1] = target
