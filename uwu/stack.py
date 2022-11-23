93b386c568f46c6af283796ce4e5739a297299fa
Sofia N. Lima <me@dzshn.xyz>
2022-11-23 13:43:16 -0300

    feat: more stack trolling

--- uwu/stack.py
+++ uwu/stack.py
@@ -1,30 +1,49 @@
 import ctypes
 import types
 from collections.abc import Sequence
+from typing import Union
+
+svoid_p = ctypes.sizeof(ctypes.c_void_p)
 
 
 class Stack(Sequence):
     def __init__(self, frame: types.FrameType):
-        self.address = id(frame) + ctypes.sizeof(ctypes.c_void_p) * 44
+        self.address = id(frame) + svoid_p * 44
         self.size = frame.f_code.co_stacksize
 
-    def __getitem__(self, key: int) -> object:
+    def __getitem__(self, key: Union[int, slice]) -> object:
         if isinstance(key, int):
             if key < 0:
                 key += self.size
-            return ctypes.py_object.from_address(
-                self.address + key * ctypes.sizeof(ctypes.c_void_p)
-            ).value
+            return ctypes.py_object.from_address(self.address + key * svoid_p).value
         if isinstance(key, slice):
             seq = []
             for i in range(*key.indices(self.size)):
                 seq.append(
-                    ctypes.py_object.from_address(
-                        self.address + i * ctypes.sizeof(ctypes.c_void_p)
-                    ).value
+                    ctypes.py_object.from_address(self.address + i * svoid_p).value
                 )
             return seq
         return NotImplemented
 
+    def __setitem__(self, key: int, value: object) -> None:
+        if not isinstance(key, int):
+            return NotImplemented
+
+        ctypes.c_void_p.from_address(self.address + key * svoid_p).value = id(value)
+
+    def __delitem__(self, key: int) -> None:
+        if not isinstance(key, int):
+            return NotImplemented
+
+        ctypes.c_void_p.from_address(self.address + key * svoid_p).value = 0
+
     def __len__(self) -> int:
         return self.size
+
+    def __invert__(self):
+        i = 0
+        while i < self.size:
+            if ctypes.c_void_p.from_address(self.address + i * svoid_p).value is None:
+                break
+            i += 1
+        return ctypes.py_object.from_address(self.address + (i - 1) * svoid_p).value
